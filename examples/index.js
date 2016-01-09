3307285d07a4b8f85402168ef335395f825126ab
Azim Gadzhiagayev <me@azimgd.com>
2016-01-09 17:05:00 +0100

    ADD ES6 compilation support

--- examples/index.js
+++ examples/index.js
@@ -6,16 +6,16 @@ const username = process.env.username;
 const password = process.env.password;
 
 bot.auth(username, password)
-.then((headers) => bot.setLike(headers, '1156352422809813646'));
-
-bot.auth(username, password)
-.then((headers) => bot.setComment(headers, '1156408808103072490_518286907', 'somehow this comment must be ignored, i guess!'));
-
-bot.auth(username, password)
-.then((headers) => bot.unsetFollow(headers, '29738951'));
-
-bot.auth(username, password)
-.then((headers) => bot.setFollow(headers, '29738951'));
-
-bot.auth(username, password)
-.then((headers) => bot.getFeedByHashtag(headers, 'cats'));
+.then((headers) => bot.setLike(headers, '1157751993106715465_5932067'));
+//
+// bot.auth(username, password)
+// .then((headers) => bot.setComment(headers, '1156408808103072490_518286907', 'somehow this comment must be ignored, i guess!'));
+//
+// bot.auth(username, password)
+// .then((headers) => bot.unsetFollow(headers, '29738951'));
+//
+// bot.auth(username, password)
+// .then((headers) => bot.setFollow(headers, '29738951'));
+//
+// bot.auth(username, password)
+// .then((headers) => bot.getFeedByHashtag(headers, 'cats'));
---
+++ src/init.js
@@ -0,0 +1,54 @@
+import Http from './services/http';
+
+const auth = (username, password) => {
+  return (new Http)
+    .grabTokens()
+    .then((res) => new Http(res).auth(username, password))
+    .catch(() => false);
+};
+
+const setLike = (headers, mediaId) => {
+  return (new Http(headers))
+    .setLike(mediaId)
+    .catch(() => false);
+};
+
+const setComment = (headers, mediaId, text) => {
+  return (new Http(headers))
+    .setComment(mediaId, text)
+    .catch(() => false);
+};
+
+const setFollow = (headers, userId) => {
+  return (new Http(headers))
+    .setFollow(userId)
+    .catch(() => false);
+};
+
+const unsetFollow = (headers, userId) => {
+  return (new Http(headers))
+    .unsetFollow(userId)
+    .catch(() => false);
+};
+
+const getSelfFeed = (headers, startCursor = null, count = 12) => {
+  return (new Http(headers))
+    .getSelfFeed(startCursor, count)
+    .catch(() => false);
+};
+
+const getFeedByHashtag = (headers, hashtag, startCursor = null, count = 12) => {
+  return (new Http(headers))
+    .getFeedByHashtag(hashtag, startCursor, count)
+    .catch(() => false);
+};
+
+export default {
+  auth,
+  setLike,
+  setComment,
+  setFollow,
+  unsetFollow,
+  getSelfFeed,
+  getFeedByHashtag,
+};
