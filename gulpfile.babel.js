2e0df5c84cea33ac1847e1c7511945c7db88c889
Azim Gadzhiagayev <me@azimgd.com>
2016-06-27 12:29:23 +0200

    Example compilation

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,14 +1,23 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
+import rename from 'gulp-rename';
 
-gulp.task('babel', () => {
+gulp.task('babel-example', () => {
+  return gulp.src('./examples/example.js')
+    .pipe(babel())
+    .pipe(rename('index.js'))
+    .pipe(gulp.dest('./examples'));
+});
+
+gulp.task('babel-source', () => {
   return gulp.src('./src/**/*.js')
     .pipe(babel())
     .pipe(gulp.dest('./lib'));
 });
 
 gulp.task('watch', () => {
-  gulp.watch('./src/**/*.js', ['babel']);
+  gulp.watch('./examples/example.js', ['babel-example']);
+  gulp.watch('./src/**/*.js', ['babel-source']);
 });
 
 gulp.task('default', ['watch']);
